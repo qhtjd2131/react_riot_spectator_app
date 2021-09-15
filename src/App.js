718d1959d0b13962ad8bcbd3c7c5952803363285
qhtjd2131 <qhtjd2131@gmail.com>
2021-09-15 23:14:35 +0900

    api 를 사용하여 암호화된 id 데이터 가져오기

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { React, useState, useEffect } from "react";
+import axios from "axios";
+
+//https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/%ED%95%B5%EC%82%AC%EC%A3%BD?api_key=RGAPI-c5622e97-caa4-40ab-8515-0bf6150af985
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const summonerName = [
+    "쿡%20덕",
+    "핵사죽",
+    "worldberry",
+    "티모는과학입ll다",
+    "배함비",
+  ];
+
+  const [isLoading, setIsLoading] = useState(true);
+  const [summonerEcryptedId, setSummonerEcryptedId] = useState([]);
+  let ecryptedId = [];
+
+  //const [data, setData] = useState([]);
+  // name.map((summonerName) => {
+  //   const { data } =
+  //     axios.get(`https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/쿡덕?api_key=RGAPI-c5622e97-caa4-40ab-8515-0bf6150af985
+  //   `);
+  //   console.log(data);
+  // });
+
+  const getEncryptedId = () => {
+    summonerName.map(async (smName) => {
+      const {
+        data: { id },
+      } = await axios.get(
+        `https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/${smName}?api_key=RGAPI-c5622e97-caa4-40ab-8515-0bf6150af985`
+      );
+      ecryptedId.push(id);
+    });
+    console.log(ecryptedId);
+  };
+
+  useEffect(() => {
+    getEncryptedId();
+    setIsLoading(false);
+  });
+
+  return <div> hello world </div>;
 }
 
 export default App;
