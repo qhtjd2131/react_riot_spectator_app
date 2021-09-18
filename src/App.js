86b83790c254836f406ab19769051f0f13ebd7ca
qhtjd2131 <qhtjd2131@gmail.com>
2021-09-18 17:32:19 +0900

    api를 활용해 암호화된id가져오기

--- src/App.js
+++ src/App.js
@@ -1,47 +1,80 @@
 import { React, useState, useEffect } from "react";
 import axios from "axios";
-
-//https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/%ED%95%B5%EC%82%AC%EC%A3%BD?api_key=RGAPI-c5622e97-caa4-40ab-8515-0bf6150af985
+import InGame from "./components/InGame";
+import "./App.css";
 
 function App() {
-  const summonerName = [
-    "쿡%20덕",
-    "핵사죽",
-    "worldberry",
-    "티모는과학입ll다",
-    "배함비",
-  ];
-
+  const [user, setUser] = useState([
+    {
+      id: undefined,
+      nickName: "쿡 덕",
+      onGame: false,
+    },
+    {
+      id: undefined,
+      nickName: "핵사죽",
+      onGame: false,
+    },
+    {
+      id: undefined,
+      nickName: "worldberry",
+      onGame: false,
+    },
+    {
+      id: undefined,
+      nickName: "티모는과학입ll다",
+      onGame: false,
+    },
+    {
+      id: undefined,
+      nickName: "배함비",
+      onGame: false,
+    },
+  ]);
   const [isLoading, setIsLoading] = useState(true);
-  const [summonerEcryptedId, setSummonerEcryptedId] = useState([]);
-  let ecryptedId = [];
-
-  //const [data, setData] = useState([]);
-  // name.map((summonerName) => {
-  //   const { data } =
-  //     axios.get(`https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/쿡덕?api_key=RGAPI-c5622e97-caa4-40ab-8515-0bf6150af985
-  //   `);
-  //   console.log(data);
-  // });
-
-  const getEncryptedId = () => {
-    summonerName.map(async (smName) => {
-      const {
-        data: { id },
-      } = await axios.get(
-        `https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/${smName}?api_key=RGAPI-c5622e97-caa4-40ab-8515-0bf6150af985`
-      );
-      ecryptedId.push(id);
-    });
-    console.log(ecryptedId);
-  };
 
   useEffect(() => {
-    getEncryptedId();
-    setIsLoading(false);
-  });
+    getEncryptedId().then(() => {
+      console.log(user);
+      getUserData();
+      setIsLoading(false);
+    });
+  }, []);
 
-  return <div> hello world </div>;
+  const getEncryptedId = async () => {
+    return Promise.all(
+      user.map(async (user) => {
+        user.id = (
+          await axios.get(
+            `https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/${user.nickName}?api_key=RGAPI-cf5e5172-5e01-445e-b754-b03e22a8901e`
+          )
+        ).data.id;
+      })
+    );
+  };
+
+  const getUserData = async (userIds) => {
+    //encrypted id 를 이용하여 다시 api 호출
+  };
+
+  return (
+    <section className="container">
+      {isLoading ? (
+        <div className="loader">loading ...</div>
+      ) : (
+        <div className="main">
+          {user.map((user) => (
+            <InGame
+              key={user.id}
+              id={user.id}
+              nickName={user.nickName}
+              onGame={user.onGame}
+            />
+          ))}
+        </div>
+      )}
+    </section>
+  );
 }
 
 export default App;
---
+++ src/components/InGame.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function InGame({ id }) {
+  return <h3>{id}...</h3>;
+}
+
+export default InGame;
